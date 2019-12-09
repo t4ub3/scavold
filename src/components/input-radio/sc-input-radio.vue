d82165d5b54694c559c2bc0ed9b1fa2c6d2c5a01
t4ub3 <jn.jonasneumann@googlemail.com>
2019-12-09 17:41:30 +0100

    button hover, radio styling (buggy)

--- src/components/input-radio/sc-input-radio.vue
+++ src/components/input-radio/sc-input-radio.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="sc-input-radio">
-    <input type="radio"
+    <input class="sc-input-radio sc-input-radio__input"
+            type="radio"
            :value="value"
            :name="name"
            @change="$emit('select', $event.target.value)"
            v-model="selectedValue" />
-    <label>{{ label }}</label>
+    <label class="sc-input-radio sc-input-radio__label">
+      {{ label }}
+    </label>
   </div>
 </template>
 
@@ -40,6 +43,23 @@
   @import '@scavold/core/styles/global';
 
   .sc-input-radio {
-    
+    @include sa-input-radio;
+
+    &__input {
+      display: none;
+    }
+
+    &__label {
+      padding-left: 24px;
+      position: relative;
+      &::before {
+        position: absolute;
+        left: $distance-xs;
+        top: $distance-xs;
+        height: 10px;
+        width: 10px;
+        border: solid 2px black;
+      }
+    }
   }
 </style>
