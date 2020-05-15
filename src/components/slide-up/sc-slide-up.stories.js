cd337b11043bb10f5290a65430dd0dff41167e3d
t4ub3 <jn.jonasneumann@googlemail.com>
2020-05-15 18:24:55 +0200

    clean up

--- src/components/slide-up/sc-slide-up.stories.js
+++ src/components/slide-up/sc-slide-up.stories.js
@@ -1,6 +1,6 @@
 import ScSlideUp from "./sc-slide-up.vue";
 import { buildTemplate } from "../../../.storybook/helpers";
-import { withKnobs, text, select, object } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 const lorem =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
